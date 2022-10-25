7364dc88a7f0736ce3daf6e88845ed91f541ecf8
Glauco Todesco <glauco.todesco@gmail.com>
2022-10-25 17:24:56 -0300

    Load Pokemon details page

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -3,10 +3,8 @@
 loadPokemonDetails();
 
 function loadPokemonDetails(){
-    
     const params = new URLSearchParams(window.location.search);
-    console.log("Number =" + params.get("number"));
-    
+   
     if(params.has("number"))
     {
         let number = params.get("number");
